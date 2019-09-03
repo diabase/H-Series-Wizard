cb4ee978616518ad863881c7ce9d80fd8c2f68ff
Manuel Coenen <manuel.coenen@gmail.com>
2019-09-03 10:56:45 +0200

    Release 1.0.2

--- DiabasePrintingWizard/frmMain.cs
+++ DiabasePrintingWizard/frmMain.cs
@@ -16,7 +16,7 @@ namespace DiabasePrintingWizard
 {
     public partial class FrmMain : Form
     {
-        public static readonly string version = "v1.0.2-dev";
+        public static readonly string version = "v1.0.2";
         public static readonly NumberFormatInfo numberFormat = CultureInfo.CreateSpecificCulture("en-US").NumberFormat;
 
         private Duet.Observer observer;
